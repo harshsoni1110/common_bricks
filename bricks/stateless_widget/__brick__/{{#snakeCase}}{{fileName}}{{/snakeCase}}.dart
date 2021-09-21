922e28117788e792a50c1e698f9eef2824e0b1e2
harsh <harshsoni111093@gmail.com>
2021-09-21 10:07:24 +0530

    Feat: Stateless widget brick added

---
+++ bricks/stateless_widget/__brick__/{{#snakeCase}}{{fileName}}{{/snakeCase}}.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class {{#pascalCase}}{{fileName}}{{/pascalCase}} extends StatelessWidget {
+  const {{#pascalCase}}{{fileName}}{{/pascalCase}}({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
